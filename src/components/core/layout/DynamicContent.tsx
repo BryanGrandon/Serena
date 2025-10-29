8fdce21bd61b1b7d6ac8c61cb5d599468298e026
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-10-29 12:45:10 -0300

    refactor(web):change DynamicContent and [category]_[id]

--- src/components/core/layout/DynamicContent.tsx
+++ src/components/core/layout/DynamicContent.tsx
@@ -3,13 +3,7 @@ type Props = {
   type?: string
 }
 
-import general from '../../../data/general.json'
-
 const DynamicContent = ({ data, type }: Props) => {
-  let img = ''
-  if (type == 'perfumes-for-women') img = general.perfums_img.ladies['100ml']
-  if (type == 'perfumes-for-men') img = general.perfums_img.gentlemen['100ml']
-
   return (
     <>
       <aside style={{ gridArea: 'aside' }} className='text-black min-w-60 p-4 bg-[#f5e1af] rounded-r-3xl shadow-lg shadow-black/20'>
@@ -33,15 +27,15 @@ const DynamicContent = ({ data, type }: Props) => {
         <article className='grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] gap-4'>
           {data.map((el) => (
             <section
-              key={el.name}
+              key={el?.name}
               className='flex flex-row items-center rounded-xl overflow-hidden bg-white/45 shadow-lg shadow-black/20 text-black hover:scale-[1.02] transition-transform duration-150 ease-in-out cursor-pointer'
             >
-              <img src={img} className='h-20' alt={`img-${el.name}`} />
+              <img src={el?.img} className='h-20' alt={`img-${el?.name}`} />
               <section className='relative overflow-hidden px-4'>
-                <p className='flex flex-col text-xl'>{el.name}</p>
-                <abbr title={el.inspiration} className='no-underline'>
+                <p className='flex flex-col text-xl'>{el?.name}</p>
+                <abbr title={el?.inspiration} className='no-underline'>
                   <sub className='text-gray-600'>inspiration</sub>
-                  <h3 className='overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer '>{el.inspiration}</h3>
+                  <h3 className='overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer '>{el?.inspiration}</h3>
                 </abbr>
               </section>
             </section>
