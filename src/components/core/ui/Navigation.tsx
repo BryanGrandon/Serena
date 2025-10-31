bc88d8b19fc9024d8169285cd812d9cdbea3df2f
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-10-31 17:15:53 -0300

    refactor(web):change pages, data and components

--- src/components/core/ui/Navigation.tsx
+++ src/components/core/ui/Navigation.tsx
@@ -3,23 +3,31 @@ import general from '../../../data/general.json' assert { type: 'json' }
 const Navigation = () => {
   const navigation = general.navigation
 
-  const list = [
-    { id: 1, name: navigation.home.label, href: navigation.home.url },
-    { id: 2, name: navigation.perfumes.label, href: navigation.perfumes.url },
-    { id: 3, name: navigation.colonies.label, href: navigation.colonies.url },
-    { id: 4, name: navigation.creams.label, href: navigation.creams.url },
-  ]
-
   return (
-    <nav className='w-full flex items-center justify-between p-4'>
-      <a href={navigation.home.url}>{general.logo}</a>
-      <section className='flex gap-5'>
-        {list.map((item) => (
-          <a key={item.id} href={item.href} className='hover:text-primary'>
-            {item.name}
+    <nav className='text-black p-4'>
+      <article className='flex w-full items-center justify-between gap-8'>
+        <a href={navigation.home.url} className='min-w-fit font-bold'>
+          {general.logo}
+        </a>
+        <section className='flex w-full border border-black rounded-lg overflow-hidden '>
+          <input type='text' placeholder='Escribe lo que buscas...' className='py-1 px-2 w-full rounded-l-lg' />
+          <a href='#' className=' border-l rounded-r-lg py-1 px-2'>
+            Search
           </a>
-        ))}
-      </section>
+        </section>
+        <button>|||</button>
+      </article>
+      <article>
+        <ul className='flex gap-8 mt-4'>
+          {Object.values(navigation).map((navItem) => (
+            <li key={navItem.url}>
+              <a href={navItem.url} className='hover:underline'>
+                {navItem.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </article>
     </nav>
   )
 }
--- src/utilities/scripts/add-image-in-content.ts
+++ src/utilities/scripts/add-image-in-content.ts
@@ -9,13 +9,13 @@ const addImageInContent = ({ items, type }: add_image_in_content) => {
   items.map((el) => {
     switch (type) {
       case 'F':
-        el.img = PERFUMES_IMG.women['100ml']
+        el.img = PERFUMES_IMG.women.box
         break
       case 'H':
-        el.img = PERFUMES_IMG.men['100ml']
+        el.img = PERFUMES_IMG.men.box
         break
       case 'Q':
-        el.type === 'red' ? (el.img = PERFUMES_IMG.niche.red['100ml']) : (el.img = PERFUMES_IMG.niche.black['100ml'])
+        el.type === 'red' ? (el.img = PERFUMES_IMG.niche.red.box) : (el.img = PERFUMES_IMG.niche.black.box)
         break
       case 'QB':
         el.img = PERFUMES_IMG.niche.black['100ml']
